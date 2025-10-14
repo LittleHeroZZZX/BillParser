53b6d642c7a08bbe979cd08651755116a13e5d09
zhouxin <zhou.xin@mail.ustc.edu.cn>
2025-10-14 23:35:58 +0800

    feat: use Dynaconf as config parser

---
+++ src/billparser/config.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+from dynaconf import Dynaconf
+
+ROOT_DIR = Path(__file__).parent.parent.parent
+CONFIG_ROOT = ROOT_DIR / "config"
+all_yaml_files = CONFIG_ROOT.glob("**/*.yaml")
+to_load_yaml_files = [
+    str(file) for file in all_yaml_files if not file.name.endswith(".example.yaml")
+]
+
+settings = Dynaconf(
+    envvar_prefix="BILLPARSER",
+    settings_files=to_load_yaml_files,
+    ignore_unknown_envvars=True,
+    merge_enabled=True,
+)
