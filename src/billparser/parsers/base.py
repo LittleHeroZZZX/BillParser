8a42ce8038d2b83fae0ecdc0fccd01bafe2e4ac1
zhouxin <zhou.xin@mail.ustc.edu.cn>
2025-10-27 23:56:32 +0800

    Refactor: extract in/output type with get_args

--- src/billparser/parsers/base.py
+++ src/billparser/parsers/base.py
@@ -1,6 +1,6 @@
 # src/billparser/parsers/base.py
 from abc import ABC, abstractmethod
-from typing import TypeVar
+from typing import TypeVar, get_args, get_origin
 
 from ..models import Bill, RawImage, RawText
 
@@ -19,13 +19,27 @@ class BaseParser[T_Input, T_Output](ABC):
     def __init__(self):
         pass
 
+    def _get_parser_generic_args(self) -> tuple[type, type]:
+        """
+        Retrieve the concrete types used for T_Input and T_Output.
+        """
+        if hasattr(self, "_cached_base_args"):
+            return self._cached_base_args
+        for base in self.__class__.__mro__:
+            origin = get_origin(base)
+            if origin is BaseParser:
+                args = get_args(base)
+                self._cached_base_args = args
+                return args
+        raise TypeError("Could not determine generic arguments for BaseParser")
+
     @property
     def input_type(self) -> type[T_Input]:
         """
         Input data type that this parser accepts.
         (Automatically derived from the Generic[T_Input, ...])
         """
-        return self.__orig_bases__[0].__args__[0]  # type: ignore[attr-defined]
+        return self._get_parser_generic_args()[0]
 
     @property
     def output_type(self) -> type[T_Output]:
@@ -33,7 +47,7 @@ class BaseParser[T_Input, T_Output](ABC):
         Output data type that this parser produces.
         (Automatically derived from the Generic[..., T_Output])
         """
-        return self.__orig_bases__[0].__args__[1]  # type: ignore[attr-defined]
+        return self._get_parser_generic_args()[1]
 
     @abstractmethod
     async def parse(self, input_data: T_Input) -> T_Output:
