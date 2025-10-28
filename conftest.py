4c476f4f2504401762feacc8c041010a9d4c253b
zhouxin <zhou.xin@mail.ustc.edu.cn>
2025-10-28 18:01:29 +0800

    Feat: fixture settings before test

--- conftest.py
+++ conftest.py
@@ -5,15 +5,15 @@ import pytest
 from billparser.config import settings
 
 TEST_CONFIG_ROOT = Path(__file__).parent / "tests" / "config"
-all_yaml_files = TEST_CONFIG_ROOT.glob("**/*.yaml")
+# skip settings.yaml to avoid cover model secrets env
+all_yaml_files = [
+    str(file)
+    for file in TEST_CONFIG_ROOT.glob("**/*.yaml")
+    if not file.name.endswith("settings.yaml")
+]
 
 
 @pytest.fixture(scope="session", autouse=True)
 def override_dynaconf_settings():
     """Override Dynaconf settings for tests."""
-    settings.configure(
-        envvar_prefix="BILLPARSER",
-        settings_files=[str(file) for file in all_yaml_files],
-        merge_enabled=True,
-        ignore_unknown_envvars=False,
-    )
+    settings.load_file(all_yaml_files)
